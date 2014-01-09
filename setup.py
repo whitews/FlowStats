4b1440ab33bde55092e5cc59390c2839dba067df
Scott White <whitews@gmail.com>
2014-01-09 14:01:52 -0500

    added distutils setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='FlowStats',
+    version='0.1',
+    packages=['flowstats'],
+    package_data={'': []},
+    description='Flow Cytometry Standard Statistical Functions',
+    requires=['numpy', 'scipy', 'dpmix'],
+)
