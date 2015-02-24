6d6e403d01a2a0b84493621dec1f07e40e51511d
Scott White <whitews@gmail.com>
2015-02-24 10:58:40 -0500

    bump version to 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='FlowStats',
-    version='0.1',
+    version='0.2',
     packages=['flowstats'],
     package_data={'': []},
     description='Flow Cytometry Standard Statistical Functions',
